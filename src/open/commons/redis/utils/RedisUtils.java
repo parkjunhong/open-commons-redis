24d96e46e56982c79d8b62cfe35bdff986b0eb5d
Park_Jun_Hong <fafanmama@naver.com>
2019-03-29 14:29:55 +0900

    'CHANGE' LICENSE files.
    before: MIT
    after: Apache License, Vversion 2.0

--- src/open/commons/redis/utils/RedisUtils.java
+++ src/open/commons/redis/utils/RedisUtils.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 Park Jun-Hong_(fafanmama_at_naver_com)
+ * Copyright 2011 Park Jun-Hong (parkjunhong77/gmail/com)
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
